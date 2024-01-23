f639f6230db79b1a80396978b7fbdbf43b68d247
Ankit Meena <63893740+ankitmeena007@users.noreply.github.com>
2024-01-23 18:26:04 +0530

    Update index.js
    attempted to reduce lag between successive presses

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-var crash= new Audio("sounds/crash.mp3");
-var kick= new Audio("sounds/kick-bass.mp3");
-var snare= new Audio("sounds/snare.mp3");
-var tom1= new Audio("sounds/tom-1.mp3");
-var tom2= new Audio("sounds/tom-2.mp3");
-var tom3= new Audio("sounds/tom-3.mp3");
-var tom4= new Audio("sounds/tom-4.mp3");
 
 
 var button_list=document.querySelectorAll(".drum");
-  for (var i=0; i<button_list.length;i++) {
-    button_list[i].addEventListener("click",function(){
-      var buttontext=this.innerHTML;
-      onClick(buttontext);
-      buttonAnimation(buttontext);
-    });
+for (var i=0; i<button_list.length;i++) {
+  button_list[i].addEventListener("click", function(){
+    
+    var buttontext=this.innerHTML;
+    
+    onClick(buttontext);
+    
+    buttonAnimation(buttontext);
+    
+});
 
 }
 
 // for keyboard buttons so that they also play the instrument
-document.addEventListener("keydown",function(eventt){
+document.addEventListener('keydown',function(eventt){
 
   onClick(eventt.key);
   buttonAnimation(eventt.key);
@@ -33,24 +30,37 @@ function onClick(key1){
 
   switch (key1) {
     case "l":
+      var crash= new Audio("sounds/crash.mp3");
       crash.play();
       break;
+      
     case "k":
+      var kick= new Audio("sounds/kick-bass.mp3");
       kick.play();
       break;
+      
     case "j":
+      var snare= new Audio("sounds/snare.mp3");
       snare.play();
       break;
+      
     case "w":
+      var tom1= new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
+      
     case "a":
+      var tom2= new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
+      
     case "s":
+      var tom3= new Audio("sounds/tom-3.mp3");
       tom3.play();
       break;
+      
     case "d":
+      var tom4= new Audio("sounds/tom-4.mp3");
       tom4.play();
       break;
 
@@ -65,10 +75,12 @@ function onClick(key1){
 
 function buttonAnimation(currentKey) {
   var activeButton=document.querySelector("."+currentKey);
-  activeButton.classList.add("pressed");
-  setTimeout(function(){activeButton.classList.remove("pressed")},100);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+      activeButton.classList.remove("pressed");
+    },100);
   //syntax: setTimeout(function,milliseconds,param1 opt ,param2 opt ...)
-}
+    }
 
 
 
